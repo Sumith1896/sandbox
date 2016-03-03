356e58b2881c5cb54a18f89aa64f412b6cbdecd1
Sumith1896 <sumith1896@gmail.com>
2016-03-03 18:37:29 +0530

    Test

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-# inside this git repo we'll pretend to be a new user
-git config user.name "travis"
-git config user.email "nobody@nobody.com"
-
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet origin gh-pages #> /dev/null 2>&1
+if !  egrep " $" -nr .; then
+    echo No trailing whitespace;
+else
+    exit -1;
+fi
